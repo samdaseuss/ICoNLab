244aa974a7f2cdfc41197a3d78d8a8439b494176
samdaseuss <ohj3423@naver.com>
2024-02-11 17:39:06 +0900

    0.0.4 version

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,4 @@
+import { Noto_Serif_KR, Gothic_A1 } from 'next/font/google';
 import '../styles/globals.scss';
 import { Provider } from "react-redux";
 import store from "../../store";
@@ -5,7 +6,20 @@ import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
 import { SessionProvider } from "next-auth/react";
 import Head from 'next/head';
+
+
 let persistor = persistStore(store);
+
+const notoSerifKr = Noto_Serif_KR({
+  weight: ['500', '900'],
+  subsets: ['latin'],
+});
+
+const gothic = Gothic_A1({
+  weight: ['500'],
+  subsets: ['latin'],
+});
+
 
 function MyApp({ Component, pageProps: {session, ...pageProps} }) {
   return (
@@ -15,13 +29,15 @@ function MyApp({ Component, pageProps: {session, ...pageProps} }) {
         <meta name="description" content="ICoN LAB" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <SessionProvider session={session}>
-        <Provider store={store}>
-          <PersistGate Loading={null} persistor={persistor}>
-            <Component {...pageProps} />
-          </PersistGate>
-        </Provider>
-      </SessionProvider>
+      <main className={gothic.className}>
+        <SessionProvider session={session}>
+          <Provider store={store}>
+            <PersistGate Loading={null} persistor={persistor}>
+              <Component {...pageProps} />
+            </PersistGate>
+          </Provider>
+        </SessionProvider>
+      </main>
     </>
   );
 }
